5515e2b9d57969ec3e117474ca0af62718c7326c
litbe <manabe@mabu-no-MacBook-Air.local>
2016-05-04 00:34:25 +0900

    JavaScript
    delete_sample

--- 3/3.1/addEventListener.js
+++
@@ -1,8 +0,0 @@
-/* loadイベントリスナーをセット */
-dom.event.addEventListener(window, 'load', initDocument);
-
-/* HTML文書が読み込まれたときに実行させる処理 */
-function initDocument() {
-  /* アラートウィンドウ表示 */
-  alert("success!");
-}
